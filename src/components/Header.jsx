b9e9fe82ffd9f0283216706b986f473f06e49371
Gabriela Patiño <gabyapd05@gmail.com>
2024-03-20 15:26:57 -0400

    letter spacing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             <img src={logo} alt="Logo BallScore" />
         </div>
         <nav className="hidden ss:flex font-raleway uppercase font-bold leading-3 text-sm text-white">
-            <ul className="flex flex-row gap-x-11">
+            <ul className="flex flex-row gap-x-11 tracking-widest">
                 <li className="hover:text-primary"><a href="#home">Home</a></li>
                 <li className="hover:text-primary"><a href="#resultados">Resultados</a></li>
                 <li className="hover:text-primary"><a href="#equipos">Equipos</a></li>
@@ -25,7 +25,7 @@ const Header = () => {
             onClick={() => setToggle((prev) => !prev)}
           />
           <div className={`${toggle ? 'flex' : 'hidden'} font-raleway uppercase font-bold leading-3 text-sm p-4 bg-primary absolute top-14 right-0 mx-4 my-2 min-w-[140px]  rounded-xl menu`}>
-            <ul className="list-none flex flex-col justify-end items-center flex-1 gap-y-4">
+            <ul className="list-none flex flex-col justify-end items-center flex-1 gap-y-4 tracking-widest">
               <li className="hover:text-white"><a href="#home">Home</a></li>
               <li className="hover:text-white"><a href="#resultados">Resultados</a></li>
               <li className="hover:text-white"><a href="#equipos">Equipos</a></li>
