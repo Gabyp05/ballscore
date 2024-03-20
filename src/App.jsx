574ed0e47720650921c61844ddeb4d4222f46dc4
Gabriela Patiño <gabyapd05@gmail.com>
2024-03-20 13:08:27 -0400

    add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@
 function App() {
 
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <p className="font-bold">probando font raleway</p>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF9142",
+        bgBlack: "#1A1A1A",
+        whiteGray: "#C5BFBF",
+        paragraph: "#A8A8A8",
+        dark: "#131313",        
+      },
+      fontFamily: {
+        syne: ["Syne Variable", "sans-serif"],
+        raleway: ["Raleway Variable", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 }
