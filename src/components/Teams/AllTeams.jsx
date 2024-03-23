2dc47d249de274157cdb8a96e92b0021a90decf4
Gabyp05 <gabyapd05@gmail.com>
2024-03-23 12:22:08 -0400

    show all teams

--- src/components/Teams/AllTeams.jsx
+++ src/components/Teams/AllTeams.jsx
@@ -1,15 +1,85 @@
-import React from 'react'
-import {construccion} from "../../assets"
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom"
+import { teamLogos } from '../../data';
+import { national, american} from "../../assets"
 
 const Teams = () => {
+
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      try{
+        const response = await fetch('https://baseball4.p.rapidapi.com/v1/mlb/teams', {
+            method: 'GET',
+            headers: {
+            'X-RapidAPI-Key': '425a2f8650msh2c93977c1d9775fp1d700djsnccb1675c3877',
+            'X-RapidAPI-Host': 'baseball4.p.rapidapi.com'
+            }
+        });
+        let data = await response.json();
+        if (data.body) {
+            data = Object.values(data.body);
+        }
+        console.log(data);
+        setTeams(data);
+      }
+      catch (error) {
+        console.error(`Error al obtener los equipos: ${error.message}`);
+      };
+    }
+
+    fetchTeams();
+  }, []);
+
+  const americanLeagueTeams = teams.filter(team => team.league.id === 103);
+  const nationalLeagueTeams = teams.filter(team => team.league.id === 104);
+  
   return (
     <section id='resultados' className='container mx-auto py-16 mb-80 flex flex-col items-center justify-center'>
         <div className="max-w-4xl flex flex-col items-center justify-center pb-8 gap-10">
             <h1 className='font-syne font-bold text-4xl text-center text-white mb-2.5'>
-                Sección en construcción
+                Equipos
             </h1>
-            <img src={construccion} alt="Sitio en construcción" className='px-4 rounded-[3rem]' />
-        </div>  
+        </div> 
+        <div>
+        <main className="grid grid-cols-1 ss:grid-cols-2 gap-24 md:gap-80">
+          <article>
+            <div className="flex flex-row items-center gap-4 mb-10">
+              <img src={american} alt="Liga Americana" className='size-[60px]' />
+              <h1 className='text-white font-raleway text-2xl'>Liga Americana</h1>  
+            </div>
+              <div className="flex flex-col gap-8"> 
+                {americanLeagueTeams.map((team, index) => ( 
+                  <Link to={`/team/${team.teamName}`}>
+                    <div key={index} className="flex flex-row gap-4 items-center">
+                      <img src={teamLogos[team?.id]?.src} alt={teamLogos[team?.id]?.alt}  className='h-[40px] md:h-[50px] object-contain'/>
+                      <h2 className='font-raleway text-white text-xs md:text-[18px] text-center font-medium'>{team.name}</h2>
+                    </div>
+                  </Link>    
+                ))} 
+              </div>  
+          </article>
+          <article>
+          <div className="flex flex-row items-center gap-4 mb-10">
+              <img src={national} alt="Liga Nmericana" className='size-[60px]' />
+              <h1 className='text-white font-raleway text-2xl'>Liga Nacional</h1>  
+            </div>
+            <div className="flex flex-col gap-8">  
+              {nationalLeagueTeams.map((team, index) => (   
+                <Link to={`/team/${team.teamName}`}>
+                  <div key={index} className="flex flex-row gap-4 items-center">
+                    <img src={teamLogos[team?.id]?.src} alt={teamLogos[team?.id]?.alt}  className='h-[40px] md:h-[50px] object-contain'/>
+                    <h2 className='font-raleway text-white text-xs md:text-[18px] text-center font-medium'>{team.name}</h2>
+                  </div> 
+                </Link> 
+              ))}
+            </div>  
+          </article>
+        </main>
+    </div>
+
+        
     </section>
   )
 }
