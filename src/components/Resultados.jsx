3c17c7ee0f3f431abb3ebe62f80feea070b7b06d
Gabriela Patiño <100980917+Gabyp05@users.noreply.github.com>
2024-04-09 19:08:53 -0400

    Update Resultados.jsx

--- src/components/Resultados.jsx
+++ src/components/Resultados.jsx
@@ -14,7 +14,7 @@ const Resultados = () => {
         const response = await fetch(`https://baseball4.p.rapidapi.com/v1/mlb/schedule?date=${formattedDate}`, {
             method: 'GET',
             headers: {
-            'X-RapidAPI-Key': '425a2f8650msh2c93977c1d9775fp1d700djsnccb1675c3877',
+            'X-RapidAPI-Key': '759c6b95d4msh112e4efe0c17b03p148719jsnedff2d29596a',
             'X-RapidAPI-Host': 'baseball4.p.rapidapi.com'
             }
         });
